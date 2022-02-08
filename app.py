138d12a0b644b3e3f03bb5f8b0117e08742a3755
Farid Karimli <karimli.farid@icloud.com>
2022-02-07 23:43:46 -0500

    Edited login and register

--- app.py
+++ app.py
@@ -88,14 +88,7 @@ def new_page_function():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
 	if flask.request.method == 'GET':
-		return '''
-			   <form action='login' method='POST'>
-				<input type='text' name='email' id='email' placeholder='email'></input>
-				<input type='password' name='password' id='password' placeholder='Enter your password'></input>
-				<input type='submit' name='submit' value="Log in"></input>
-			   </form></br>
-		   <a href='/'>Home</a>
-			   '''
+		return render_template('login.html')
 	#The request method is POST (page is recieving data)
 	email = flask.request.form['email']
 	cursor = conn.cursor()
